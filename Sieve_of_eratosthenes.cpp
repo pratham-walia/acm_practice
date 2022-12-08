cf8cb4903acba6162ad3affb598a021140e9efb5
pratham-walia <112483326+pratham-walia@users.noreply.github.com>
2022-12-08 11:29:04 +0530

    Add files via upload

---
+++ Sieve_of_eratosthenes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i=2;i<n;i++)
+    {
+        int c=0;
+        for (int j=2;j<i;j++)
+        {
+            if (i%j==0)
+            {
+                c=c+1;
+            }
+
+        }
+        if (c==0)
+        {
+            cout<<i<<"\n";
+        }
+    }
+    return 0;
+}
